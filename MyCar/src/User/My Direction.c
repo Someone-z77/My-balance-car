7d11e1684f4125a77cdbb6d6a13a30e0a85d90ea
Someone-z77 <60023178+Someone-z77@users.noreply.github.com>
2020-04-18 18:21:11 +0800

    Update My Direction.c
    差比和之后传出来数据为  nan

--- MyCar/src/User/My Direction.c
+++ MyCar/src/User/My Direction.c
@@ -1,36 +1,76 @@
 #include "include.h"
 
 
-float g_fDirectionControl_Out;  
+float g_fDirectionControl_Out; 
+float Flag_circle1=0,Flag_circle2=0;
+//uint16_t  ADC_max[4]={0},ADC_min[4]={10000};
+extern uint16_t  ADC_max[4]={0},ADC_min[4]={10000};
 
-uint16_t  AD_left[3]={0,0,0},AD_right[3]={0,0,0};
-
-float  Direct_error[3]={0,0,0};
+float  left_value[2]={0},right_value[2]={0};
+float  Direct_error[2]={0};
 
 /*--------------------------------------------------------------------*/
-void Direction_Control(void)//方向控制
+/*读取最大值，最小值，在赛道多处移动*/
+void AD_max_min()//采集五百次
 {
+   uint16_t ad_get[4]={0};
+   int i=0,j=0;
+   for(j=0;j<500;j++)
+   {
+       ad_get[0]=ADC_Get(0);
+       ad_get[1]=ADC_Get(1);
+       ad_get[2]=ADC_Get(2);
+       ad_get[3]=ADC_Get(3);   
+      for(i=0;i<4;i++)
+      {
+          if(ad_get[i]>=ADC_max[i])
+         {
+            ADC_max[i]=ad_get[i];
+         }
+         if(ad_get[i]<=ADC_min[i])
+         {
+            ADC_min[i]=ad_get[i];
+         }
+         printf("max:  %d",ADC_max[i]);
+         printf("     min:  %d\n",ADC_min[i]);
+      }
+   }
+}
+void Read_adc(void)
+{    
+  
+     uint16_t  AD_left[2]={0},AD_right[2]={0};
      /*左右水平电感采集 */
-     AD_left[0]=ADC_Get(0);
-     AD_left[1]=ADC_Get(1);
-     AD_right[0]=ADC_Get(3);
-     AD_right[1]=ADC_Get(4);           //这里注意位置要对应！！！
-     
+     AD_left[0]=ADC_Get(1);
+     AD_right[0]=ADC_Get(2);
      /*左右竖直电感采集*/
-     AD_left[2]=ADC_Get(2);
-     AD_right[2]=ADC_Get(5);
+     AD_left[1]=ADC_Get(0);
+     AD_right[1]=ADC_Get(3);           //这里注意位置要对应！！！
+     
+     
      
      /*归一化=（测量值-最小值）/（最大值-最小值）*/
+     left_value[0]=(AD_left[0]-ADC_min[1])/(ADC_max[1]-ADC_min[1]);
+     left_value[1]=(AD_left[1]-ADC_min[0])/(ADC_max[0]-ADC_min[0]);
+     right_value[0]=(AD_right[0]-ADC_min[2])/(ADC_max[2]-ADC_min[2]);
+     right_value[1]=(AD_right[1]-ADC_min[3])/(ADC_max[3]-ADC_min[3]);
      
      
      /*差比和---水平*/
-     Direct_error[0]=(float)(AD_left[0]-AD_right[0])/(AD_left[0]+AD_right[0]);
-     Direct_error[1]=(float)(AD_left[1]-AD_right[1])/(AD_left[1]+AD_right[1]);
-     
+     Direct_error[0]=(float)(left_value[0]-right_value[0])/(left_value[0]+right_value[0]);
+     //Direct_error[0]=(Direct_error[0]>1?1:Direct_error[0]);
+     //Direct_error[0]=(Direct_error[0]<-1?-1:Direct_error[0]);
      /*差比和---竖直*/
-     Direct_error[2]=(float)(AD_left[2]-AD_right[2])/(AD_left[2]+AD_right[2]);
+     Direct_error[1]=(float)(left_value[1]-right_value[1])/(left_value[1]+right_value[1]);
+     //Direct_error[1]=(Direct_error[1]>1?1:Direct_error[1]);
+     //Direct_error[1]=(Direct_error[1]<-1?-1:Direct_error[1]);
      
-     //if()
+     printf("%.2f",Direct_error[0]);
+     printf("     %.2f\n",Direct_error[1] );
+      
+}
+void Direction_Control(void)//方向控制
+{
 }
 void DirectionControl_Output(void)//方向输出平滑函数
 {
