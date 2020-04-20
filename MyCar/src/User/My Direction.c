85afbcd0e0e2c4acdf2ebf94bf72592116c5f2de
Someone-z77 <60023178+Someone-z77@users.noreply.github.com>
2020-04-20 20:23:47 +0800

    采集电感
    采集最小值有点问题

--- MyCar/src/User/My Direction.c
+++ MyCar/src/User/My Direction.c
@@ -3,15 +3,15 @@
 
 float g_fDirectionControl_Out; 
 float Flag_circle1=0,Flag_circle2=0;
-//uint16_t  ADC_max[4]={0},ADC_min[4]={10000};
-extern uint16_t  ADC_max[4]={0},ADC_min[4]={10000};
+
+uint16_t  ADC_max[4]={0},ADC_min[4]={55};
 
 float  left_value[2]={0},right_value[2]={0};
 float  Direct_error[2]={0};
 
 /*--------------------------------------------------------------------*/
 /*读取最大值，最小值，在赛道多处移动*/
-void AD_max_min()//采集五百次
+void AD_max()//采集五百次
 {
    uint16_t ad_get[4]={0};
    int i=0,j=0;
@@ -27,18 +27,33 @@ void AD_max_min()//采集五百次
          {
             ADC_max[i]=ad_get[i];
          }
+         printf("max:  %d \n  ",ADC_max[i]);
+      }
+   }
+}
+void AD_min()
+{
+  uint16_t ad_get[4]={0};
+   int i=0,j=0;
+   for(j=0;j<500;j++)
+   {
+       ad_get[0]=ADC_Get(0);
+       ad_get[1]=ADC_Get(1);
+       ad_get[2]=ADC_Get(2);
+       ad_get[3]=ADC_Get(3);   
+      for(i=0;i<4;i++)
+      {
          if(ad_get[i]<=ADC_min[i])
          {
             ADC_min[i]=ad_get[i];
          }
-         printf("max:  %d",ADC_max[i]);
          printf("     min:  %d\n",ADC_min[i]);
       }
    }
 }
 void Read_adc(void)
 {    
-  
+
      uint16_t  AD_left[2]={0},AD_right[2]={0};
      /*左右水平电感采集 */
      AD_left[0]=ADC_Get(1);
@@ -46,31 +61,39 @@ void Read_adc(void)
      /*左右竖直电感采集*/
      AD_left[1]=ADC_Get(0);
      AD_right[1]=ADC_Get(3);           //这里注意位置要对应！！！
-     
-     
+     printf("l0:  %d  ",AD_left[0]);
+     printf("l1:  %d  ",AD_left[1]);
+     printf("r0:  %d  ",AD_right[0]);
+     printf("r1:  %d\n",AD_right[1]);
      
      /*归一化=（测量值-最小值）/（最大值-最小值）*/
      left_value[0]=(AD_left[0]-ADC_min[1])/(ADC_max[1]-ADC_min[1]);
      left_value[1]=(AD_left[1]-ADC_min[0])/(ADC_max[0]-ADC_min[0]);
      right_value[0]=(AD_right[0]-ADC_min[2])/(ADC_max[2]-ADC_min[2]);
      right_value[1]=(AD_right[1]-ADC_min[3])/(ADC_max[3]-ADC_min[3]);
+     printf("v0:  %.3f  ",left_value[0]);
+     printf("v1:  %.3f  ",left_value[1]);
+     printf("v2:  %.3f  ",right_value[0]);
+     printf("v3:  %.3f\n",right_value[1]);
      
      
      /*差比和---水平*/
-     Direct_error[0]=(float)(left_value[0]-right_value[0])/(left_value[0]+right_value[0]);
+     Direct_error[0]=(left_value[0]-right_value[0])/(left_value[0]+right_value[0]);
      //Direct_error[0]=(Direct_error[0]>1?1:Direct_error[0]);
      //Direct_error[0]=(Direct_error[0]<-1?-1:Direct_error[0]);
      /*差比和---竖直*/
-     Direct_error[1]=(float)(left_value[1]-right_value[1])/(left_value[1]+right_value[1]);
+     Direct_error[1]=(left_value[1]-right_value[1])/(left_value[1]+right_value[1]);
      //Direct_error[1]=(Direct_error[1]>1?1:Direct_error[1]);
      //Direct_error[1]=(Direct_error[1]<-1?-1:Direct_error[1]);
      
      printf("%.2f",Direct_error[0]);
-     printf("     %.2f\n",Direct_error[1] );
-      
+     printf("     %.2f\n\n",Direct_error[1] );
+     
 }
 void Direction_Control(void)//方向控制
 {
+  
+  
 }
 void DirectionControl_Output(void)//方向输出平滑函数
 {
--- MyCar/src/User/main.c
+++ MyCar/src/User/main.c
@@ -1,22 +1,21 @@
-/*  【Crystal】 50.000Mhz
-    【busclock】 40.000MHz
-    【pllclock】200.000MHz
-    ICM20602 硬件SPI接口,读取速度比较快，大概20us 
-    GND   ------   GND
-    3-5V  ------   3.3V
-    SCL   ------   PTE2
-    SDA   ------   PTE1
-    SA    ------   PTE3
-    CS    ------   PTE0*/
 
 #include "include.h" 
-
 void main(void)
 {   
 	
       PLL_Init(PLL180);
       NVIC_SetPriorityGrouping(0x07 - 2); //4个抢占优先级 4个子优先级 
-      Test_ICM20602();
-     
+      Motor_Enc_Init();
+      UART_Init(UART4, 115200);
+      ADC_Init(ADC0);
+      ADC_Init(ADC1);
+      //PIT0_IRQHandler();
+      AD_max();
+      AD_min();
+      while(1)
+      {
+        Read_adc();
+        //AD_Get_Go();
+      }
 	
 }                                                
