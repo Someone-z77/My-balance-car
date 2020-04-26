abc279ad9532664538aa94a30a4c6dc1a7ad9f58
Someone-z77 <60023178+Someone-z77@users.noreply.github.com>
2020-04-26 17:11:39 +0800

    调试pid

--- MyCar/src/User/My Direction.c
+++ MyCar/src/User/My Direction.c
@@ -3,11 +3,11 @@
 
 float g_fDirectionControl_Out; 
 float Flag_circle1=0,Flag_circle2=0;
-
+short pwmR,pwmL;
 uint16_t  ADC_max[4]={0},ADC_min[4]={1000,1000,1000,1000};
 
 float  left_value[2]={0},right_value[2]={0};
-float  Direct_error[2]={0};
+float  Direct_error[2]={0},Error;
 
 /*--------------------------------------------------------------------*/
 /*读取最大值，最小值，在赛道多处移动*/
@@ -15,8 +15,8 @@ void AD_max_min()//采集500次
 {
    uint16_t ad_get[4]={0};
    int i=0,j=0;
-   for(j=0;j<500;j++)
-   {
+   //for(j=0;j<2000;j++)
+   //{
        ad_get[0]=ADC_Get(0);
        ad_get[1]=ADC_Get(1);
        ad_get[2]=ADC_Get(2);
@@ -26,20 +26,15 @@ void AD_max_min()//采集500次
           if(ad_get[i]>=ADC_max[i])
          {
             ADC_max[i]=ad_get[i];
-         }
-          if(ad_get[i]<=ADC_min[i])
-         {
-            ADC_min[i]=ad_get[i];
-         }
-         printf("max:  %d \n  ",ADC_max[i]);
-         printf("min:  %d \n  ",ADC_min[i]);
+         }        
       }
-   }
+   //}
+   //printf("max:  %d   %d   %d   %d\n  ",ADC_max[0],ADC_max[1],ADC_max[2],ADC_max[3]);
 }
 
 void Read_adc(void)
 {    
-
+     AD_max_min();
      uint16_t  AD_left[2]={0},AD_right[2]={0};
      /*左右水平电感采集 */
      AD_left[0]=ADC_Get(1);
@@ -49,10 +44,20 @@ void Read_adc(void)
      AD_right[1]=ADC_Get(3);           //这里注意位置要对应！！！
     
      /*归一化=（测量值-最小值）/（最大值-最小值）*/
-     left_value[0]=(AD_left[0]-ADC_min[1])/(ADC_max[1]-ADC_min[1]);
-     left_value[1]=(AD_left[1]-ADC_min[0])/(ADC_max[0]-ADC_min[0]);
-     right_value[0]=(AD_right[0]-ADC_min[2])/(ADC_max[2]-ADC_min[2]);
-     right_value[1]=(AD_right[1]-ADC_min[3])/(ADC_max[3]-ADC_min[3]);
+     //left_value[0]=(AD_left[0]-ADC_min[1])/(ADC_max[1]-ADC_min[1]);
+     //left_value[1]=(AD_left[1]-ADC_min[0])/(ADC_max[0]-ADC_min[0]);
+     //right_value[0]=(AD_right[0]-ADC_min[2])/(ADC_max[2]-ADC_min[2]);
+     //right_value[1]=(AD_right[1]-ADC_min[3])/(ADC_max[3]-ADC_min[3]);
+     /*归一化  最大值100，最小值1*/
+     left_value[0]=AD_left[0]*100/ADC_max[1];
+     left_value[1]=AD_left[1]*100/ADC_max[0];
+     right_value[0]=AD_right[0]*100/ADC_max[2];
+     right_value[1]=AD_right[1]*100/ADC_max[3];
+     
+     left_value[0]=(left_value[0]<1?1:left_value[0]);
+     left_value[1]=(left_value[1]<1?1:left_value[1]);
+     right_value[0]=(right_value[0]<1?1:right_value[0]);
+     right_value[1]=(right_value[1]<1?1:right_value[1]);
      
      if((left_value[0]+right_value[0]!=0)&&(left_value[1]+right_value[1]!=0))//防止出现nan情况
      {
@@ -63,16 +68,19 @@ void Read_adc(void)
          /*差比和---竖直*/
          Direct_error[1]=(left_value[1]-right_value[1])/(left_value[1]+right_value[1]);
          Direct_error[1]=(Direct_error[1]>1?1:Direct_error[1]);
-         Direct_error[1]=(Direct_error[1]<-1?-1:Direct_error[1]);
-     
-        printf("%.2f  ",Direct_error[0]);
-        printf("  %.2f\n",Direct_error[1] );
+         Direct_error[1]=(Direct_error[1]<-1?-1:Direct_error[1]);       
      }
+     printf("%.2f  ",Direct_error[0]);
+     printf("  %.2f\n",Direct_error[1] );
      
 }
 void Direction_Control(void)//方向控制
 {
-  
+      Error=(0.4*Direct_error[1]+0.6*Direct_error[0])*500;
+      pwmR=2500+Error;
+      pwmL=3050-Error;
+      MOTOR_Ctrl(1,pwmL);
+      MOTOR_Ctrl(2,pwmR);
   
 }
 void DirectionControl_Output(void)//方向输出平滑函数
--- MyCar/src/User/My Speed.c
+++ MyCar/src/User/My Speed.c
@@ -1,12 +1,11 @@
 #include "include.h" 
 
 
-int16 Left_pulse,Right_pluse;
-float g_Lspeed=0,g_Rspeed=0;//编码器脉冲
-float g_RealSpd=0;//测量速度
-float g_ExpectSpd=0;//期望速度   记得初始化
-float Speed_out;//pid之后的输出
 
+float g_Lspeed=0,g_Rspeed=0;//编码器脉冲
+int16 g_RealSpd_R=0,g_RealSpd_L=0;//测量速度
+int16 g_ExpectSpd_R=1000,g_ExpectSpd_L=0;//期望速度   记得初始化
+int16 Speed_out_R,Speed_out_L;//pid之后的输出ort
 struct // pid 结构体
 {
     float error_old;
@@ -16,7 +15,7 @@ struct // pid 结构体
     float i;
     float d;
     
-}PID;
+}PID={0,0,0,1,0,0};
 
  
 float g_fSpeedControlOut_New;            //  SpeedControl_Output
@@ -29,8 +28,8 @@ float g_fSpeedControl_Out;
 int16  read_CNT_R()//这里需要确定一下左右编码器对应的管jio
 {
     short val;   
-    val = FTM_CNT_REG(FTM1);    
-    FTM_CNT_REG(FTM1) = 0;    //清空标志位         
+    val = FTM_CNT_REG(FTM2);    
+    FTM_CNT_REG(FTM2) = 0;    //清空标志位         
 
     return val;
 }
@@ -45,26 +44,34 @@ int16 read_CNT_L()
 /*---------------------------------------------------------*/
 void Get_speed()//速度获取
 {
-     Left_pulse = read_CNT_L();
-     Right_pluse = read_CNT_R();
-     
-     //先来一个小小的滤波
+     short Left_pulse,Right_pluse;
+          MOTOR_Ctrl(1,2500);
+          MOTOR_Ctrl(2,2500);
+     Left_pulse = FTM_ABGet(FTM1);
+     Right_pluse = -FTM_ABGet(FTM2);//注意这里的正负
+     //printf("\r\n/ENC1 %5d \r\n",Left_pulse);
+     //printf("\r\n/ENC2 %5d \r\n",Right_pluse);
+          delayms(100);
+    // 先来一个小小的滤波
      g_Lspeed = 0.3*Left_pulse +0.7*g_Lspeed;
      g_Rspeed = 0.3*Right_pluse  +0.7*g_Rspeed;
      
-     //接下来是计算速度,也有一个小小的滤波
-     g_RealSpd=0.1*g_RealSpd+0.3*(g_Lspeed+g_Rspeed)*0.5;//这后面少了一系列的数！！！
+     g_RealSpd_L=0.4*g_RealSpd_L+0.6*g_Lspeed;
+     g_RealSpd_R=0.4*g_RealSpd_R+0.6*g_Rspeed;
        
 }
 void Speed_PID()//pid控制速度
 {
      //
      PID.error_old = PID.error_new;
-     PID.error_new =  g_ExpectSpd-g_RealSpd;
+     PID.error_new =  g_ExpectSpd_R-g_RealSpd_R;
      PID.integral+=PID.error_new;
      
-     Speed_out= PID.p*PID.error_new+PID.i*PID.integral+PID.d*(PID.error_new-PID.error_old);
+     g_ExpectSpd_R= PID.p*PID.error_new+PID.i*PID.integral+PID.d*(PID.error_new-PID.error_old);//积分限幅
      //输出pwm吧
+     MOTOR_Ctrl(2,g_RealSpd_R);
+     Send(g_ExpectSpd_R,g_RealSpd_R);
+
 }
 
 void Speed_Control()//速度控制
@@ -83,6 +90,7 @@ void SpeedControl_Output(void)//速度输出平滑函数
    
    g_fSpeedControl_Out = fValue * 
          (g_nSpeedControlPeriod+1) /90 + g_fSpeedControlOut_Old;
+   
 }
 
 
